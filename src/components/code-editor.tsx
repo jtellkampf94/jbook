40d57efc5389ae6716901af203c153c28da69158
Jonathan <jtellkampf@gmail.com>
2021-04-30 23:57:10 +0100

    Added code formatting feature to code editor component using prettier package

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
-import MonacoEditor, { OnChange } from "@monaco-editor/react";
+import { useRef } from "react";
+import MonacoEditor, { OnChange, OnMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,29 +9,52 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>();
+
   const handleChange: OnChange = (value, event) => {
     if (value) onChange(value);
   };
 
+  const handleEditorDidMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
+  };
+
+  const handleFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    });
+
+    editorRef.current.setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      onChange={handleChange}
-      value={initialValue}
-      theme="vs-dark"
-      language="javascript"
-      height="500px"
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        tabSize: 2
-      }}
-    />
+    <div>
+      <button onClick={handleFormatClick}>Format</button>
+      <MonacoEditor
+        onMount={handleEditorDidMount}
+        onChange={handleChange}
+        value={initialValue}
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2
+        }}
+      />
+    </div>
   );
 };
 
