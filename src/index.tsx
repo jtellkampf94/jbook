df3cf4b45f635c495f3ab3bf098645298184f359
Jonathan <jtellkampf@gmail.com>
2021-05-01 13:08:15 +0100

    Refactored index.tsx by splitting iframe and its logic into preview.tsx component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
 
 const App: React.FC = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
   const startService = async () => {
@@ -27,8 +28,6 @@ const App: React.FC = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -40,28 +39,8 @@ const App: React.FC = () => {
       }
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data)
-            } catch (error) {
-              const root = document.querySelector('#root')
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
-              throw err;
-            }
-          }, false)
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -69,19 +48,10 @@ const App: React.FC = () => {
         initialValue="const a = 1;"
         onChange={value => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={e => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      ></iframe>
+      <Preview code={code} />
     </div>
   );
 };
