e06aac33807da807f96685083d38a573c6265b5a
Jonathan <jtellkampf@gmail.com>
2021-05-03 17:35:06 +0100

    Completed styling of action-bar including the addition of icons

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,7 +1,9 @@
+import { Fragment } from "react";
 import { Cell } from "../state/cell";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
 import ActionBar from "./action-bar";
+import "./cell-list-item.css";
 
 interface CellListItemProps {
   cell: Cell;
@@ -10,17 +12,24 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === "code") {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <Fragment>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </Fragment>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <Fragment>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </Fragment>
+    );
   }
 
-  return (
-    <div>
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
